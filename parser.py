28792a7ca3e6299f3fe52f8f4b8a7ae145d6df30
soc999 <55905093+soc999@users.noreply.github.com>
2020-06-24 22:45:07 +0900

    bugfix Travis CI

--- parser.py
+++ parser.py
@@ -1,21 +1,17 @@
 import pytest
-import glob, os
+import glob, os, sys
 
 
-def test_parse(path):
-    lines = []
-    with open(path, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-    sample_args = 0
-    for line in lines:
-        if not line.startswith("#"):
-            meta_args = len(line.split(","))
-            if sample_args == 0:
-                sample_args = meta_args
-            assert sample_args == meta_args
-
-
-for path in glob.glob(os.path.dirname(__file__) + "/*"):
-    if path.find(".") == -1 and os.path.isfile(path):
-        test_parse(path)
-
+def check_columun_count():
+    for path in glob.glob(__file__ + "/*"):
+        if path.find(".") == -1 and os.path.isfile(path):
+            lines = []
+            with open(path, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+            sample_args = 0
+            for line in lines:
+                if not line.startswith("#"):
+                    meta_args = len(line.split(","))
+                    if sample_args == 0:
+                        sample_args = meta_args
+                    assert sample_args == meta_args
